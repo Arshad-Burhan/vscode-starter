4aac47dae6717de1943844c406818ac30ab7b9e7
Arshad Burhan <arshad.burhan47@gmail.com>
2020-11-27 21:32:41 +0530

    First commit

---
+++ test1.py
@@ -0,0 +1,20 @@
+import math
+import sys
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello " + str(who_to_greet)
+    return greeting
+
+
+print(greet("World"))
+print(greet("Buru"))
+print(greet("Git0"))
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+print(r.ok)
